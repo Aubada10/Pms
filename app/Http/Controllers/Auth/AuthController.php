123be0adb233ffe20e6c950a2ffe7bd588b7a5cd
Obada <obada.tr@gmail.com>
2023-05-03 15:07:58 +0300

    first commit

---
+++ app/Http/Controllers/Auth/AuthController.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Auth;
+use App\Utils\ImageUpload;
+use Illuminate\Http\Request;
+use App\Http\Requests\Auth\RegisterUserRequest;
+use App\Http\Requests\Auth\LogInRequest;
+
+
+
+class AuthController extends Controller
+{
+
+    public function register_user(RegisterUserRequest $request)
+    {
+        $image = ImageUpload::imageUpload($request->image,100,200,'profile/');
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password),
+            'phone_number'=>$request->phone_number,
+            'image'=>$image,
+            ]);
+        return response()->json([
+            'status'=>true,
+            'message'=>'User created successfully',
+            'id'=>$user->id,
+            'token'=>$user->createToken('Api Token')->plainTextToken,
+        ],200);;
+    }
+    function login(LogInRequest $request)
+    {
+            if(!Auth::attempt($request->only(['email','password'])))
+            {
+                return response()->json([
+                    'status'=>false,
+                    'message'=>'email or password does not match ,',
+                    ''=>$request->errors(),
+                ],401);
+            }
+            $user = User::where('email', $request->email)->first();
+            return response()->json([
+                'status'=>true,
+                'token'=>$user->createToken('Api Token')->plainTextToken,
+                'role'=>$user->role,
+                'id'=>$user->id
+            ],200);
+        }
+        function logout(Request $request)
+        {
+            $user = $request->user();
+            $user->tokens()->delete();
+            return 'logout  successfully';
+        }
+    }
---
+++ app/Http/Requests/Auth/RegisterUserRequest.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Requests\Auth;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class RegisterUserRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'name'=>'string|min:4|max:12',
+            'email'=>'required|email',
+            'password'=>'required|min:8|max:10|confirmed',
+            'phone_number'=>'required|numeric|starts_with:09|digits:10',
+            'image'=>'nullable|image|mimes:jpeg,png,jpg,gif,svg|max:2048',
+        ];
+    }
+}
