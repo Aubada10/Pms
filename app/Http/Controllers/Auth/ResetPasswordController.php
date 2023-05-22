7bcd8848adb994d2c0ee3fee5d76ee0dff635cb7
Obada <obada.tr@gmail.com>
2023-05-22 20:43:49 +0300

    edit on reset password

--- app/Http/Controllers/Auth/ResetPasswordController.php
+++ app/Http/Controllers/Auth/ResetPasswordController.php
@@ -7,23 +7,11 @@ use App\Notifications\ResetPasswordVerificationNotification;
 use App\Http\Requests\Auth\ResetPasswordRequest;
 use Illuminate\Support\Facades\Hash;
 use App\Models\User;
-use Otp;
 
 class ResetPasswordController extends Controller
 {
-    private $otp;
-    public function __construct()
-    {
-        $this->otp = new Otp;
-    }
+    public function __construct(){}
     public function resetPassword(ResetPasswordRequest $request){
-        $otp2 = $this->otp->validate($request->email,$request->otp);
-        if(! $otp2->status)
-        {
-            return response()->json([
-                ['error' =>$otp2]
-            ],401);
-        }
         $user = User::where('email',$request->email)->first();
         $user->update(['password'=>Hash::make($request->password)]);
         $user->tokens()->delete();
